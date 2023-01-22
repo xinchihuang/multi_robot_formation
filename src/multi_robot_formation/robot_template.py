08e1c5fbf863de9e6c46bbb2715c6c7dd3dc8519
xinchi <xhuang60@stevens.edu>
2023-01-22 13:55:39 -0500

    change file structure

--- src/multi_robot_formation/robot_template.py
+++ src/multi_robot_formation/robot_template.py
@@ -15,7 +15,7 @@ class Robot:
     A robot template. Used for handling different components and store data for components.
     """
 
-    def __init__(self,platform="vrep",controller_type="model"):
+    def __init__(self,sensor,executor,controller,platform="vrep",controller_type="model"):
         self.index = None
         self.GNN_model = None
         self.sensor_data = None
@@ -24,10 +24,9 @@ class Robot:
 
         self.platform = platform
         self.controller_type = controller_type
-        self.sensor = None
-        self.executor = Executor()
-        self.controller = Controller()
-
+        self.sensor = sensor
+        self.executor = executor
+        self.controller = controller
     def get_sensor_data(self):
         """
         Read sensor data from sensor in simulator/realworld
@@ -42,19 +41,19 @@ class Robot:
         :return: Control data
         """
         if self.controller_type == "expert":
-            self.control_data = self.controller.centralized_control(
+            model_data = self.controller.centralized_control(
                 self.index, self.sensor_data, self.scene_data
             )
         elif self.controller_type == "model":
-            # expert_data = self.controller.centralized_control(
-            #     self.index, self.sensor_data, self.scene_data
-            # )
-            # model_data = self.controller.decentralized_control(
-            #     self.index, self.sensor_data, self.scene_data, number_of_agents=5
-            # )
-            # model_data = self.controller.decentralized_control_pose(
-            #     self.index, self.sensor_data, self.scene_data, number_of_agents=5
-            # )
+            model_data = self.controller.decentralized_control(
+                self.index, self.sensor_data, self.scene_data, number_of_agents=5
+            )
+            # mode
+        elif self.controller_type == "model_pose":
+            model_data = self.controller.decentralized_control_pose(
+                self.index, self.sensor_data, self.scene_data, number_of_agents=5
+            )
+        elif self.controller_type == "model_dummy":
             # model_data = self.controller.decentralized_control_dummy(
             #     self.index, self.sensor_data, self.scene_data, number_of_agents=3
             # )
@@ -66,11 +65,8 @@ class Robot:
             #     print("position list", self.scene_data.position_list)
             #     print("orientation list", self.scene_data.orientation_list)
             # print("expert ", expert_data.velocity_x, expert_data.velocity_y)
-            print("model ", model_data.velocity_x, model_data.velocity_y)
-            self.control_data=model_data
-            # self.control_data = self.controller.decentralized_control(
-            #     self.index, self.sensor_data, self.scene_data, number_of_agents=5
-            # )
+        print("model ", model_data.velocity_x, model_data.velocity_y)
+        self.control_data=model_data
 
         return self.control_data
 
