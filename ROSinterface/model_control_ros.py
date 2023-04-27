f8ea8298ad706574cfdd2a7c5911940bc7d3f1d3
xinchi <xhuang60@stevens.edu>
2023-04-27 11:53:08 -0400

    fix import problem

--- ROSinterface/model_control_ros.py
+++ ROSinterface/model_control_ros.py
@@ -35,7 +35,6 @@ class ModelControl:
             executor=Executor(),
             controller=self.controller,
             platform="robomaster",
-            model_path=self.model_path
         )
 
         self.topic = topic
