85f0b71e067298e452bad438131db981cb376cf3
xinchi <xhuang60@stevens.edu>
2023-05-07 16:17:33 -0400

    fix model import

--- ROSinterface/model_control_ros.py
+++ ROSinterface/model_control_ros.py
@@ -24,9 +24,8 @@ from cmvision_3d.msg import Blobs3d, Blob3d
 class ModelControl:
     def __init__(self, topic):
 
-        self.model_path = os.path.join(
-           "../src/multi_robot_formation/src/multi_robot_formation/saved_model/vit0.9.pth"
-        )
+        self.model_path = "../src/multi_robot_formation/src/multi_robot_formation/saved_model/vit0.9.pth"
+
         self.desired_distance=1.0
         self.controller=VitController(model_path=self.model_path,desired_distance=self.desired_distance)
         self.robot = Robot(
