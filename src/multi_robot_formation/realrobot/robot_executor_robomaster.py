54ae8d76c4d24fae71748293ea9ade9cb98261f0
xinchi <xhuang60@stevens.edu>
2023-07-03 17:05:07 -0400

    add rotation when stop and timer

--- src/multi_robot_formation/realrobot/robot_executor_robomaster.py
+++ src/multi_robot_formation/realrobot/robot_executor_robomaster.py
@@ -167,11 +167,11 @@ class Executor:
             self.idle_frame+=1
             if self.idle_frame>10:
                 msg = "chassis speed x {speed_x} y {speed_y} z {speed_z}".format(
-                    speed_x=velocity_x, speed_y=velocity_y, speed_z=10
+                    speed_x=velocity_x, speed_y=velocity_y, speed_z=20
                 )
             else:
                 msg = "chassis speed x {speed_x} y {speed_y} z {speed_z}".format(
-                    speed_x=velocity_x, speed_y=velocity_y, speed_z=10
+                    speed_x=velocity_x, speed_y=velocity_y, speed_z=20
                 )
         self.connector.send_to_robot(msg)
     def stop(self):
