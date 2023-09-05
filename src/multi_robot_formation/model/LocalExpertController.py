fc9c145a0e7c85fd038bb99d9fd9966dc32070b3
xinchi <xhuang60@stevens.edu>
2023-09-05 15:21:20 -0400

    test LocalExpertController.py on real robot

--- src/multi_robot_formation/model/LocalExpertController.py
+++ src/multi_robot_formation/model/LocalExpertController.py
@@ -80,7 +80,7 @@ class LocalExpertController:
         velocity_sum_x = 0
         velocity_sum_y = 0
         velocity_sum_omega = 0
-        print(robot_id,neighbor_list)
+        print(robot_id,neighbor_list,gabreil_graph_local)
         for neighbor_id in range(len(neighbor_list)):
             if neighbor_id == robot_id or neighbor_list[neighbor_id] == 0:
                 continue
