07ee96e537f99e58d58b1032d24a007f3037646f
xinchi <xhuang60@stevens.edu>
2023-10-30 14:21:34 -0400

    fix remote control

--- scripts/realrobots/robot_executor_robomaster.py
+++ scripts/realrobots/robot_executor_robomaster.py
@@ -141,7 +141,7 @@ class Executor:
     """
 
     def __init__(self):
-        # self.connector = UartConnector()
+        self.connector = UartConnector()
         self.idle_frame=0
 
     def execute_control(self, control_data):
