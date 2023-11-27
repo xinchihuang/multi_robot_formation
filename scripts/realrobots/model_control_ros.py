afaf0af866b2e6aca04406a15eaab79f03ce909f
xinchi <xhuang60@stevens.edu>
2023-11-27 16:13:49 -0500

    real robot experiment

--- scripts/realrobots/model_control_ros.py
+++ scripts/realrobots/model_control_ros.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-import numpy as np
+import math
 import rospy
-import sys
 import os
-import signal
-from .multi_robot_formation.realrobot.robot_executor_robomaster import Executor
+import cv2
+from .scripts.realrobot.robot_executor_robomaster import Executor
 from .multi_robot_formation.comm_data import SceneData, SensorData,ControlData
 from .multi_robot_formation.controller_new import VitController
 from .multi_robot_formation.utils.occupancy_map_simulator import MapSimulator
@@ -25,34 +24,19 @@ class ModelControl:
     def __init__(self, topic):
 
         self.model_path =os.path.abspath('..')+"/home/xinchi/catkin_ws/src/multi_robot_formation/scripts/saved_model/model_3200_epoch10.pth"
-
         self.desired_distance=1.0
-
-        # self.controller=VitController(model_path=self.model_path,desired_distance=self.desired_distance)
-
-        self.controller=LocalExpertController()
-        self.robot = Robot(
-            sensor=None,
-            executor=Executor(),
-            controller=self.controller,
-            platform="robomaster",
-        )
+        self.controller=VitController(model_path=self.model_path,desired_distance=self.desired_distance)
+        # self.controller=LocalExpertController()
 
         self.topic = topic
         # self.bridge = CvBridge()
         self.sub = rospy.Subscriber(topic, Blobs3d, self.ModelControlCallback)
         self.map_size = 100
-        self.range = 2
-        self.color_index = {"green": 0}
-        self.EP_DICT = {}
-        # self.IP_DICT = {0: "172.20.4.epoch1_3000", 1: "172.20.4.7", 2: "172.20.4.8"}
-        # self.robot.controller.initialize_GNN_model(1, self.model_path)
-        # self.IP_DICT={1:'172.20.4.7'}
-
-        # for index,ip in self.IP_DICT.items():
-        #     print('%s connecting...' % ip)
-        #     self.EP_DICT[ip] = EP(ip)
-        #     self.EP_DICT[ip].start()
+        self.sensor_range = 2
+        self.map_simulator = MapSimulator(max_x=self.sensor_range, max_y=self.sensor_range,
+                                          sensor_view_angle=math.pi * 2, local=True, partial=False)
+        self.executor=Executor()
+        self.color_index = {"Yellow": 0,"Blue": 1,"Orange": 2,"Green": 3}
     def simple_control(self,position_list,index,desired_distance):
         out_put = ControlData()
         velocity_sum_x=0
@@ -89,31 +73,20 @@ class ModelControl:
             print("no data")
             control_data=ControlData()
         else:
-            # print("position", position_list_local)
 
-            # occupancy_map_simulator = MapSimulator()
-            # occupancy_map = occupancy_map_simulator.generate_map_one(position_list_local)
-            # model_data=self.simple_control(position_list_local,0,1)
-            # self.robot.controller.num_robot=epoch5
-            # model_data=self.robot.controller.get_control(0,occupancy_map)
-            position_list_local.append([0,0,0])
-            print("position", position_list_local)
-            control_data=self.robot.controller.get_control(len(position_list_local)-1,position_list_local)
-            control_data.velocity_x = control_data.velocity_x / (len(position_list_local)-1)
-            control_data.velocity_y = control_data.velocity_y / (len(position_list_local) - 1)
-            control_data.omega = control_data.omega / (len(position_list_local) - 1)
-        self.robot.executor.execute_control(control_data=control_data)
+            occupancy_map = self.map_simulator.generate_map_one(position_list_local)
+            cv2.imshow("robot view " + str(index), np.array(occupancy_map))
+            cv2.waitKey(1)
+            data = {"robot_id": 0, "occupancy_map": occupancy_map}
+            control_data = self.controller.get_control(data)
+
+        self.executor.execute_control(control_data=control_data)
 
     def keyboard_stop(self):
         if data.data == 'q':
             self.robot.executor.stop()
             # exit(1)
             rospy.signal_shutdown("Shut down!")
-    def timed_stop(self,event):
-        print("Time's up!")
-        self.robot.executor.stop()
-        # exit(1)
-        rospy.signal_shutdown("Time's up!")
 def stop_node(event):
     rospy.signal_shutdown("Time's up!")
 if __name__ == "__main__":
