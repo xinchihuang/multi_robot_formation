216e5504cc9e2f516ee3184d7fc1a93ba4d09ef4
xinchi <xhuang60@stevens.edu>
2023-12-19 14:02:31 -0500

    real robot experiment

--- scripts/realrobots/robot_executor_robomaster.py
+++ scripts/realrobots/robot_executor_robomaster.py
@@ -155,12 +155,12 @@ class Executor:
         print("x", velocity_x)
         print("y", velocity_y)
         print("omega", omega)
-        velocity_x = 0.1 * abs(velocity_x) / velocity_x if abs(velocity_x) > 0.1 else velocity_x
-        velocity_y = 0.1 * abs(velocity_y) / velocity_y if abs(velocity_y) > 0.1 else velocity_y
+        # velocity_x = 0.1 * abs(velocity_x) / velocity_x if abs(velocity_x) > 0.1 else velocity_x
+        # velocity_y = 0.1 * abs(velocity_y) / velocity_y if abs(velocity_y) > 0.1 else velocity_y
         # omega = 0.1 * abs(omega) / omega if abs(omega) > 0.1 else omega
         # velocity_x=0
         # velocity_y=0
-        omega=0
+        # omega=0
         if velocity_x==0 and velocity_y==0 and omega==0:
             msg = "chassis speed x {speed_x} y {speed_y} z {speed_z}".format(
                 speed_x=velocity_x, speed_y=-velocity_y, speed_z=math.degrees(omega)
