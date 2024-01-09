5e3ac75e752f3c8d1cb0deef4486f19676efbf2b
xinchi <xhuang60@stevens.edu>
2024-01-08 21:15:54 -0500

    real robot uart port

--- scripts/flask.py
+++ scripts/flask.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def video_feed():
     while True:
         time.sleep(0.1)
-        return send_file('path_to_save_image/image.jpg', mimetype='image/jpeg')
+        return send_file('/home/xinchi/raw.png', mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
