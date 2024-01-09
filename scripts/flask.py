98d7e8a84266fe4457cb35b734141e8f63a19e5e
xinchi <xhuang60@stevens.edu>
2024-01-08 21:10:07 -0500

    real robot uart port

---
+++ scripts/flask.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_file
+import time
+
+app = Flask(__name__)
+
+@app.route('/video_feed')
+def video_feed():
+    while True:
+        time.sleep(0.1)  # 控制图像刷新的频率
+        return send_file('path_to_save_image/image.jpg', mimetype='image/jpeg')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
